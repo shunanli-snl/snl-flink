59f766b038b7ac9823a4264d00be74bd569b0a44
shunanli-snl <li_shunan@163.com>
2021-09-23 10:11:56 +0800

    八大数据机构 相关问题v1.1.0

---
+++ src/main/java/com/flink/demo/Application.java
@@ -0,0 +1,21 @@
+package com.flink.demo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @description:
+ * @author: lisn
+ * @date: 2021/9/23 9:06
+ * @version: V1.0
+ */
+public class Application {
+    public static void main(String[] args) {
+        int[] ints = new int[2];
+        ints[0] = 1;
+        for (int i : ints) {
+            System.out.println(i);
+        }
+NH
+    }
+}
